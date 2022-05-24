c9e4213fe2b44ab29ae21e7c2b999f0d0dd31ba2
Max Tochilashvili <100846975+mTochilashvili@users.noreply.github.com>
2022-05-24 19:54:16 +0300

    Add files via upload

---
+++ include/textgen.h
@@ -0,0 +1,20 @@
+// Copyright 2022 UNN-IASR
+#include <deque>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Markov {
+private:
+    typedef deque<string> prefix;
+    map<prefix, vector<string>> statelab;
+    int NPREF = 2;
+    int MAXGEN = 1000;
+
+public:
+    Markov(std::vector<string> allwords, int prfx_count, int gen_count);
+    Markov(std::map<prefix, vector<string>> Gen, int gen_count);
+    string TextGen();
+};
